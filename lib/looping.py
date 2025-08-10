ca105b22fadd3c5fddce48e2b97e2cb98599ce48
Rroon3y <rooneyalex2016@gmail.com>
2025-08-10 08:42:33 +0300

    add

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_list = []
+
+    for num in int_list:
+        squared_num = num ** 2
+        squared_list.append(squared_num)
+
+    return squared_list
+    print(squared_integers([1, 2, 3, 4, 5]))  
     pass
 
 def fizzbuzz():
     # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
